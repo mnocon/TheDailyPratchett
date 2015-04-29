7e57ed372e4239f49f3300c7fbeec133a92afb00
RolieOlie <markno@gmail.com>
2015-04-29 15:57:35 +0200

    Changed about message, fixed js.

--- website/script.js
+++ website/script.js
@@ -1,12 +1,9 @@
-	var $content = $("#content");
-	var $context = $("#context");
-	var $author = $("#author");	
-	
-	var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
-	var launchDate = new Date(2015, 03, 21, 12);
-	var todayDate = new Date();
-	
-	$(document).ready(function() {	
+	$(document).ready(function() {
+
+		var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+		var launchDate = new Date(2015, 03, 21, 12);
+		var todayDate = new Date();
+
 		$.getJSON( "website/quotes.json", function( data ) {
 			quotesArray = data;
 			var diffDays = Math.round(Math.abs((todayDate.getTime() - 	launchDate.getTime())/(oneDay)));
@@ -16,6 +13,10 @@
 	});
 	
 	function setQuote(i){
+	var $content = $("#content");
+	var $context = $("#context");
+	var $author = $("#author");	
+
 	$content.text(quotesArray[i].Content);
 	$author.text(quotesArray[i].Author);
 	$context.text(quotesArray[i].Context);
