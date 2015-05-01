47c79126a02c9e8f8ed9a1a6f93c9a6c59d3d6ab
RolieOlie <markno@gmail.com>
2015-05-01 23:52:07 +0200

    Add displaying the source of quote.

--- website/script.js
+++ website/script.js
@@ -1,26 +1,29 @@
-	$(document).ready(function() {
+/* global quotesArray */
+$(document).ready(function () {
 
-		var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
-		var launchDate = new Date(2015, 04, 01, 12);
-		var todayDate = new Date();
+	var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+	var launchDate = new Date(2015, 4, 1, 12);
+	var todayDate = new Date();
 
-		$.getJSON( "website/quotes.json", function( data ) {
-			quotesArray = data;
-			var diffDays = Math.round(Math.abs((todayDate.getTime() - 	launchDate.getTime())/(oneDay)));
-			var quoteToSet = diffDays % quotesArray.length;
-			setQuote(quoteToSet);
-			});
+	$.getJSON("website/quotes.json", function (data) {
+		quotesArray = data;
+		var diffDays = Math.round(Math.abs((todayDate.getTime() - launchDate.getTime()) / (oneDay)));
+		var quoteToSet = diffDays % quotesArray.length;
+		setQuote(quoteToSet);
 	});
-	
-	function setQuote(i){
+});
+
+function setQuote(i) {
 	var $content = $("#content");
 	var $context = $("#context");
-	var $author = $("#author");	
+	var $author = $("#author");
+	var $source = $("#source");
 
 	$content.text(quotesArray[i].Content);
 	$author.text(quotesArray[i].Author);
 	$context.text(quotesArray[i].Context);
-	}
+	$source.text(quotesArray[i].Source);
+}
 	
 // The world is made up of four elements: Earth, Air, Fire and Water. This is a fact well known even to Corporal Nobbs. It's also wrong. There's a fifth element, and generally it's called Surprise.
 
