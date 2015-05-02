eecd48656ba12803008777765953347f7cf726a6
RolieOlie <markno@gmail.com>
2015-05-02 15:19:16 +0200

    Accepting quote ID by url parameter. Making sure script uses gh-pages branch.

--- website/script.js
+++ website/script.js
@@ -4,16 +4,27 @@ $(document).ready(function () {
 	var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
 	var launchDate = new Date(2015, 4, 1, 12);
 	var todayDate = new Date();
-
-	$.getJSON("website/quotes.json", function (data) {
+	
+	var ID = getParameterByName('id');
+	if (ID === "")
+	{
+			$.getJSON("website/quotes.json", function (data) {
 		quotesArray = data;
 		var diffDays = Math.round(Math.abs((todayDate.getTime() - launchDate.getTime()) / (oneDay)));
-		var quoteToSet = diffDays % quotesArray.length;
-		setQuote(quoteToSet);
-	});
+		setQuote(diffDays);
+		});
+	}
+	else
+	{
+		setQuote(ID);
+	}
+
 });
 
 function setQuote(i) {
+	if (i < 0) i = 0;
+	if (i > quotesArray - 1) i % quotesArray.length;
+	
 	var $content = $("#content");
 	var $context = $("#context");
 	var $author = $("#author");
@@ -23,6 +34,13 @@ function setQuote(i) {
 	$author.text(quotesArray[i].Author);
 	$context.text(quotesArray[i].Context);
 	$source.text(quotesArray[i].Source);
+}
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 	
 // The world is made up of four elements: Earth, Air, Fire and Water. This is a fact well known even to Corporal Nobbs. It's also wrong. There's a fifth element, and generally it's called Surprise.
