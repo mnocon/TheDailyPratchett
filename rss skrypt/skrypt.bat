a7878dc897ddb83515c634ffd617e0aa75b4e8e4
RolieOlie <markno@gmail.com>
2015-05-03 16:58:05 +0200

    Removed favicon parameter.

--- rss skrypt/skrypt.bat
+++ rss skrypt/skrypt.bat
@@ -1,4 +1,4 @@
-RssGenerator "..\website\quotes.json" "The Daily Pratchett" "http://rolieolie.github.io/TheDailyPratchett/" "A quote from Sir Terry Pratchett every day." "..\website\rss.xml" "2015-04-20" "http://rolieolie.github.io/TheDailyPratchett/website/favicon.ico" "http://rolieolie.github.io/TheDailyPratchett/website/rss.xml" 5
+RssGenerator "..\website\quotes.json" "The Daily Pratchett" "http://rolieolie.github.io/TheDailyPratchett/" "A quote from Sir Terry Pratchett every day." "..\website\rss.xml" "2015-04-20" "http://rolieolie.github.io/TheDailyPratchett/website/rss.xml" 5
 
 pause
 git checkout gh-pages
