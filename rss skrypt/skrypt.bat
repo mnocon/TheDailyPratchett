c5efbe4cef071a8ecc5b1ce0ca6e8baaa275c074
RolieOlie <markno@gmail.com>
2015-09-26 14:49:40 +0200

    Modified skrypt to reflect github profile name change.

--- rss skrypt/skrypt.bat
+++ rss skrypt/skrypt.bat
@@ -1,4 +1,4 @@
-RssGenerator "..\website\quotes.json" "The Daily Pratchett" "http://rolieolie.github.io/TheDailyPratchett/" "A quote from Sir Terry Pratchett every day." "..\website\rss.xml" "2015-05-01" "http://rolieolie.github.io/TheDailyPratchett/website/rss.xml" 5
+RssGenerator "..\website\quotes.json" "The Daily Pratchett" "http://mnocon.github.io/TheDailyPratchett/" "A quote from Sir Terry Pratchett every day." "..\website\rss.xml" "2015-05-01" "http://mnocon.github.io/TheDailyPratchett/website/rss.xml" 5
 
 pause
 git checkout gh-pages
